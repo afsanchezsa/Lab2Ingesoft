d6cd1f247fa33974019ba2f6a003eded3652ba88
unknown <afsanchezsa@unal.edu.co>
2019-10-04 01:16:45 -0500

    sin oauth 2

--- src/main/java/co/edu/unal/software_engineering/labs/LabsApplication.java
+++ src/main/java/co/edu/unal/software_engineering/labs/LabsApplication.java
@@ -3,6 +3,9 @@ package co.edu.unal.software_engineering.labs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+
 @SpringBootApplication
 
 public class LabsApplication{
